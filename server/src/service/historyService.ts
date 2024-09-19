dc516f14ecb138fcab3ce02491d74b80c00deaaf
Catherine Zappia <catzappia@aol.com>
2024-09-19 12:38:10 -0400

    SERVICE_v01.01
    defined the city class and noted where the starter code is located for the HistoryService class

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,17 +1,27 @@
+// start SERVICE_v01.01 code
 // TODO: Define a City class with name and id properties
+class City {
+  name: string;
+  id: string;
+  constructor(name: string, id: string) {
+    this.name = name;
+    this.id = id;
+  }
+}
+// end SERVICE_v01.01
 
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  // private async read() {} // <- starter code
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  // private async write(cities: City[]) {} // <- starter code
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  // async getCities() {} // <- starter code
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  // async addCity(city: string) {} // <- starter code
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  // async removeCity(id: string) {}
+  // async removeCity(id: string) {} // <- starter code
 }
 
 export default new HistoryService();
