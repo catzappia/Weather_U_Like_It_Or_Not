38b4fb3a04187610cbf9a5ed663c54358235c606
Catherine Zappia <catzappia@aol.com>
2024-09-19 16:57:14 -0400

    SERVER_v01.01
    served static files and implemented middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,10 +9,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+// SERVER_v01.01 code start
 // TODO: Serve static files of entire client dist folder
+app.use(express.static('client/dist'));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// end SERVER_v01.01 code
 // TODO: Implement middleware to connect the routes
 app.use(routes);
 
