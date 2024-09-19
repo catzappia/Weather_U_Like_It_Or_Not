49f879098a747e5ad9beb7d7203f273d9d7f77de
Catherine Zappia <catzappia@aol.com>
2024-09-19 16:36:12 -0400

    SERVICE_v02.02
    completed weather service class.

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -23,27 +23,84 @@ class Weather {
 }
 // end SERVICE_v02.01 code
 
+// start SERVICE_v02.02 code
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+  private baseURL: string;
+  private apiKey: string;
+  private city: string;
+
+  constructor(apiKey: string, city: string) {
+    this.baseURL = 'https://api.openweathermap.org/data/2,5/';
+    this.apiKey = apiKey;
+    this.city = city;
+  }
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) { // <- starter code
+    const repsonse = await fetch(query);
+    return await response.json();
+  }
   // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private destructureLocationData(locationData: Coordinates): Coordinates { // <- starter code
+    const { lat, lon } = locationData;
+    return { lat, lon };
+  }
   // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
+  private buildGeocodeQuery(): string { // <- starter code
+    return `${this.baseURL}geo/1.0/direct?q=${this.city}&limit=1&appid=${this.apiKey}`;
+  }
   // TODO: Create buildWeatherQuery method
-  // private buildWeatherQuery(coordinates: Coordinates): string {}
+  private buildWeatherQuery(coordinates: Coordinates): string { // <- starter code
+    const { lat, lon } = coordinates;
+    const apiKey = process.env.WEATHER_API_KEY;
+    return `${this.baseURL}?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  }
   // TODO: Create fetchAndDestructureLocationData method
-  // private async fetchAndDestructureLocationData() {}
+  private async fetchAndDestructureLocationData() { // <- starter code
+    const query = this.buildGeocodeQuery();
+    const locationData = await this.fetchLocationData(query);
+    return this.destructureLocationData(locationData);
+  }
   // TODO: Create fetchWeatherData method
-  // private async fetchWeatherData(coordinates: Coordinates) {}
+  private async fetchWeatherData(coordinates: Coordinates) { // <- starter code
+    const query = this.buildWeatherQuery(coordinates);
+    const response = await fetch(query);
+    return await response.json();
+  }
   // TODO: Build parseCurrentWeather method
-  // private parseCurrentWeather(response: any) {}
+  private parseCurrentWeather(response: any) { // <- starter code
+    const city = response.name;
+    const temperature = response.main.temp;
+    const desccription = response.weather[0].description;
+    const icon = response.weather[0].icon;
+    return new Weather(city, temperature, description, icon);
+  }
   // TODO: Complete buildForecastArray method
-  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]) { // <- starter code
+    const forecastArray = weatherData.map((weather: any) => {
+      const date = new Date(weather.dt * 1000);
+      const temperature = weather.main.temp;
+      const description = weather.weather[0].description;
+      const icon = weather.weather[0].icon;
+      return { date, temperature, description, icon }
+    });
+    return forecastArray;
+  }
   // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+  async getWeatherForCity(city: string) { // <- starter code
+    this.city = city;
+    const coordinates = await this.fetchAndDestructureLocationData();
+    const weatherData = await this.fetchWeatherData(coordinates);
+    const currentWeather = this.parseCurrentWeather(weatherData.current);
+    const forecastArray = this.buildForecastArray(currentWeather, weatherData.daily);
+    return { currentWeather, forecastArray };
+  } 
 }
 
-export default new WeatherService();
+const apiKey = process.env.WEATHER_API_KEY || '';
+const city = 'YourCityName'; // Replace 'YourCityName' with the actual city name
+
+export default new WeatherService(apiKey, city);
+
+// end SERVICE_v02.02 code
